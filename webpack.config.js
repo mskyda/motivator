d3e25655b92887ad96c5b4f5c298e1d4b8eb6ca0
Misha Skyda <mihail.skida@gmail.com>
2019-03-08 16:09:44 +0100

    add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
 const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const DEV_DIR = path.resolve(__dirname, 'dev');
 const DIST_DIR = path.resolve(__dirname, 'dist');
 const JS_DEV_DIR = path.resolve(DEV_DIR, 'js');
 
 const config = {
-	entry : `${JS_DEV_DIR}/main.js`,
+	entry : `${JS_DEV_DIR}/main.ts`,
 	output: {
 		path    : DIST_DIR,
 		filename: 'bundle.js'
 	},
-	resolve: { extensions: [ '.js', '.ts', '.vue' ]},
+	resolve: { extensions: [ '.ts', '.vue' ] },
 	module: {
 		rules: [
 			{
+				test: /\.ts$/,
+				loader: 'ts-loader',
+				options: { appendTsSuffixTo: [/\.vue$/] }
+			},
+			{
 				test: /\.vue$/,
-				loader: 'vue-loader'
+				use: [ 'vue-loader' ]
 			},
 			{
 				test: /\.scss$/,
 				use: [
 					'vue-style-loader',
 					'css-loader',
-					{
-						loader: 'sass-loader'
-					}
+					'sass-loader'
 				]
 			}
 		]
