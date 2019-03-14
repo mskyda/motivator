fe87e291e41b5d7279c637370ec45398aab95871
Misha Skyda <mihail.skida@gmail.com>
2019-03-14 14:39:41 +0100

    add calories info

--- dev/js/app.ts
+++ dev/js/app.ts
@@ -8,14 +8,20 @@ export default Vue.extend({
 
 	data() {
 
-		console.log();
-
 		const sessionFromLocalStorage = localStorage.motivatorApp && JSON.parse(localStorage.motivatorApp);
 		const sessionFromHash = window.location.hash && checkHash(window.location.hash.substr(1));
 
 		return {
+			dataUser: null,
+			dataCalories: null,
 			dataSession: sessionFromLocalStorage || sessionFromHash
 		}
+
+	},
+
+	created(){
+
+		if(this.dataSession){ this.fetchData(); }
 
 	},
 
@@ -66,50 +72,96 @@ export default Vue.extend({
 
 		},
 
-		displayData(){
+		fetchData(){
 
 			const requestOpts = { headers: {'Authorization': `Bearer ${this.dataSession.access_token}`} };
 
-			let html = '';
+			let date : Date | string = new Date();
 
-			fetch('https://api.fitbit.com/1/user/-/profile.json', requestOpts).then(res => res.json()).then(res => {
+			date = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
 
-				if(res.errors){
+			this.fetchUserData(requestOpts);
 
-					if(res.errors[0].errorType === 'invalid_token'){ this.clearAppState(); }
+			this.fetchConsumedCalories(requestOpts, date);
 
-				}
+			this.fetchWeightData(requestOpts, date);
 
-				html += `<h2>Hello ${res.user.displayName}!</h2>`;
+			this.fetchCaloriesData(requestOpts, date);
 
-				(this.$refs.infoBox as HTMLElement).innerHTML = html; // todo: remove this dirty hack
+			// todo: steps
 
-				let date : Date | string = new Date();
+		},
 
-				date = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
+		fetchUserData(requestOpts : { headers: { [key: string]: string } }){
 
-				fetch(`https://api.fitbit.com/1/user/${this.dataSession.user_id}/body/log/weight/date/${date}/1m.json`, requestOpts).then(res => res.json()).then((res : { weight: [ { weight : number, date: string } ] }) => {
+			fetch('https://api.fitbit.com/1/user/-/profile.json', requestOpts)
+				.then(res => res.json())
+				.then(res => {
 
-					this.renderD3(res.weight);
+					if(res.errors){ this.clearAppState(); }
 
-				});
+					this.dataUser = res.user;
 
 			});
 
 		},
 
-		renderD3(data : { weight : number, date: string }[]) {
+		fetchConsumedCalories(requestOpts: { headers: { [key: string]: string } }, date : string){
+
+			fetch(`https://api.fitbit.com/1/user/${this.dataSession.user_id}/foods/log/date/${date}.json`, requestOpts)
+				.then(res => res.json())
+				.then((res) => {
+
+					// todo: check response for interesting data
+
+					this.dataCalories = res.summary.calories;
+
+				});
+
+		},
+
+		fetchWeightData(requestOpts: { headers: { [key: string]: string } }, date : string){
+
+			fetch(`https://api.fitbit.com/1/user/${this.dataSession.user_id}/body/log/weight/date/${date}/1m.json`, requestOpts)
+				.then(res => res.json())
+				.then((res : { weight: [ { weight : number, date: string } ] }) => {
+
+					// todo: check response for interesting data
+
+					this.renderD3(res.weight.map(data => ({ value: data.weight, date: new Date(data.date) })), '#weight-graph', '#0095b2');
+
+			});
+
+		},
+
+		fetchCaloriesData(requestOpts: { headers: { [key: string]: string } }, date : string){
+
+			fetch(`https://api.fitbit.com/1/user/${this.dataSession.user_id}/activities/calories/date/${date}/1m.json`, requestOpts)
+				.then(res => res.json())
+				.then((res : { 'activities-calories': [ { value : number, dateTime: string } ] }) => {
+
+					// todo: check response for interesting data
+
+					this.renderD3(res['activities-calories'].map(data => ({ value: data.value, date: new Date(data.dateTime) })), '#calories-graph', '#f4a742');
+
+				});
+
+		},
+
+		renderD3(data : { value : number, date: Date }[], svgSelector: string, lineColor: string) {
+
+			data = data.splice(0, data.length - 1); // do not consider today
 
 			const margin = 50;
-			const height = 500;
-			const width = window.innerWidth;
+			const height = 300;
+			const width = ((document.querySelector(svgSelector) as HTMLElement).parentNode as HTMLElement).offsetWidth;
 
-			const svg = d3.select('#weight')
+			const svg = d3.select(svgSelector)
 				.attr('width', width)
 				.attr('height', height);
 
-			const x = d3.scaleTime().domain(d3.extent(data, d => new Date(d.date)) as Date[]).range([margin, width - margin]);
-			const y = d3.scaleLinear().domain(d3.extent(data, d => d.weight) as number[]).nice().range([height - margin, margin]);
+			const x = d3.scaleTime().domain(d3.extent(data, d => d.date) as Date[]).range([margin, width - margin]);
+			const y = d3.scaleLinear().domain(d3.extent(data, d => d.value) as number[]).nice().range([height - margin, margin]);
 
 			svg.append('g')
 				.attr('transform', `translate(0,${height - margin})`)
@@ -121,15 +173,16 @@ export default Vue.extend({
 
 			const line = d3.line()
 				.curve(d3.curveBasis)
-				.x((d: { [key: string]: any }) => x(new Date(d.date)))
-				.y((d: { [key: string]: any }) => y(d.weight));
+				.x((d: { [key: string]: any }) => x(d.date))
+				.y((d: { [key: string]: any }) => y(d.value));
 
 			svg.append('path')
 				.datum(data)
 				.attr('fill', 'none')
-				.attr('stroke', 'steelblue')
-				.attr('stroke-width', 2)
+				.attr('stroke', lineColor)
+				.attr('stroke-width', 5)
 				.attr('d', line as any);
+
 		}
 	}
 });
--- dev/js/app.vue
+++ dev/js/app.vue
@@ -1,18 +1,29 @@
 <template>
 	<section id="app">
-		<header>
-			<h1>Fitbit Motivator</h1>
+		<header class="wrapper">
+			<h1>FITBIT MOTIVATOR</h1>
+			<a v-on:click="onLogout" href="#" v-if="dataSession">LOG OUT</a>
+			<a v-bind:href="loginUrl" v-else>LOG IN</a>
 		</header>
 		<main>
-			<section id="data-view" v-if="dataSession">
-				<a id="logout-button" v-on:click="onLogout" href="#">LOG OUT</a>
-				<div id="info-box" v-html="displayData()" ref="infoBox"></div>
-				<svg id="weight"></svg>
+			<section v-if="dataSession">
+				<h2 v-if="dataUser">Hello, {{dataUser.displayName}}!</h2>
+				<div class="wrapper">
+					<div class="weight">
+						<h3>Your weight changes:</h3>
+						<svg id="weight-graph" height="0"></svg>
+					</div>
+					<div class="calories">
+						<h3>Consumed calories today:</h3>
+						<h3 class="calories-title">{{ dataCalories }}</h3>
+					</div>
+				</div>
+				<div class="wrapper">
+					<h3>Your burned calories:</h3>
+					<svg id="calories-graph" height="0"></svg>
+				</div>
 			</section>
-			<section id="login-view" v-else>
-				<h2>Please login to Fitbit to use Motivator</h2>
-				<a id="login-button" v-bind:href="loginUrl">LOG IN</a>
-			</section>
+			<h2 v-else class="greeting">Please <a v-bind:href="loginUrl" class="login-btn">login</a> with Fitbit account to use Motivator</h2>
 		</main>
 	</section>
 </template>
@@ -24,14 +35,35 @@
 		margin: 0;
 		padding: 0;
 		font: 14px/18px Helvetica, Arial, sans-serif;
+		background: #eee;
+	}
+	a{
+		color: #0095b2;
+		font-weight: bold;
+		&:hover{
+			text-decoration: none;
+		}
+	}
+	.wrapper{
+		width: 100%;
+		overflow: hidden;
 	}
 	header{
 		border-bottom: 2px solid #ccc;
-		padding: 20px;
+		padding: 20px 0;
+		width: 100%;
+		overflow: hidden;
+		background: #fff;
 
 		h1{
-			margin: 0;
-			text-align: center;
+			margin: 0 0 0 20px;
+			color: #0095b2;
+			float: left;
+		}
+
+		a{
+			float: right;
+			margin: 0 20px 0 0;
 		}
 
 	}
@@ -40,5 +72,35 @@
 	}
 	main{
 		text-align: center;
+		padding: 20px 0 0;
+
+		h2{
+			font: 34px/50px Helvetica, Arial, sans-serif;
+			margin: 0;
+
+			&.greeting{ margin-top: 40vh; }
+
+		}
+
+		h3{
+			font: 18px/22px Helvetica, Arial, sans-serif;
+			margin: 0 0 20px;
+
+			&.calories-title{
+				font: 200px/300px Helvetica, Arial, sans-serif;
+				color: #0095b2;
+			}
+
+		}
+
+		.weight{
+			float: left;
+			width: 70%;
+		}
+		.calories{
+			float: right;
+			width: 30%;
+		}
+
 	}
 </style>
